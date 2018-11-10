5028a56241eb0b975511b004f189cfeecb89d23c
Alexander Shi <alexander_shi@hotmail.com>
2018-11-10 01:16:48 -0500

    Add ColoredEdges script with 3 different edge detector kernels and customizable colours for each, with a demonstration in a playground script.

---
+++ playground.m
@@ -0,0 +1,20 @@
+%%Image Processing Playground
+
+clear all;
+close all;
+clc;
+
+lena_fname = 'lena.tiff';
+lena = imread(lena_fname);
+lena_gray = im2double(rgb2gray(lena));
+
+figure('Name', 'Lena Gray');
+imshow(lena_gray);
+
+red = [1 0 0];
+green = [0 1 0];
+blue = [0 0 1];
+colored_edges = ColoredEdges(lena_gray, red, blue, green);
+
+figure('Name', 'Lena Edges Colored');
+imshow(colored_edges); colorbar;
